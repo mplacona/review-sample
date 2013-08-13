b4bc7a84cbafb315d98ea2d24aa65773b70125e2
Marcos Placona <marcos.placona@gmail.com>
2013-08-13 13:51:49 +0100

    Add unit tests for all exposed methods

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,4 +1,6 @@
 require "#{File.dirname(__FILE__)}/spec_helper"
+require "#{File.dirname(__FILE__)}/factories"
+
 
 describe 'main application' do
   include Rack::Test::Methods
@@ -13,7 +15,55 @@ describe 'main application' do
   end
 
   specify 'should list all reviews' do
+    #generate some fake reviews
+    50.times do |n|
+        review = Review.new(:name => "User-#{n+1}", :text => "Review -#{n+1}")
+        review.save!
+    end
+
     get '/reviews'
     last_response.should be_ok
+    
+    Review.count.should == 50
+  end
+  
+  describe "POST" do
+    specify 'should post a new review' do
+        # there should be no reviews now
+        Review.count.should == 0
+        # try and create a new review
+        post '/review', FactoryGirl.attributes_for(:review)
+        # there should now be 1 review
+        Review.count.should == 1
+    end
+  end
+
+  describe "PATCH and DELETE" do
+    before :each do
+      # create a review
+      post '/review', FactoryGirl.attributes_for(:review)
+    end
+    
+    specify 'should update a review' do
+        text = 'another test'
+            
+        # update it
+        patch '/review/1', FactoryGirl.attributes_for(:review, text: text)
+        review = Review.get(1)
+        
+        # check the updated value
+        review.text.should == text     
+    end
+
+    specify 'should delete a review' do
+        # there should be one review at this point
+        Review.count.should == 1
+
+        # Now lets delete it
+        delete 'review/1'
+
+        # There should be no more reviews now
+        Review.count.should == 0
+    end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,15 @@
 require 'factory_girl'
 
 FactoryGirl.define do
+    # Normal review
     factory :review do
         name "Joe Bloggs"
         text "Some test review"
+    end
+    
+    # A review being updated
+    factory :review1 do
+        name "Joe Bloggs"
+        text "I like reviewing this"
     end
 end
