2f41b8d6b83ff62399270ced45b8b36706637af1
Marcos Placona <marcos.placona@gmail.com>
2013-08-13 13:52:27 +0100

    Implement endpoints * GET * POST * PATCH * DELETE

--- application.rb
+++ application.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
+require 'data_mapper'
+require File.join(File.dirname(__FILE__), 'lib/review')
+
 require File.join(File.dirname(__FILE__), 'environment')
 
 configure do
@@ -20,4 +23,49 @@ end
 # root page
 get '/' do
   haml :root
+end
+
+# reviews
+get '/reviews' do
+
+end
+
+post '/review' do
+    begin
+        review = Review.new(:name => params['name'], :text => params['text'])
+        if review.save
+            review.to_json
+        else
+            error 400, review.errors.to_json
+        end
+    rescue => e
+        error 400, e.message.to_json
+    end
+end
+
+patch '/review/:id' do
+    begin
+        review = Review.get(params[:id])
+        if review.update!(:name => params['name'], :text => params['text'])
+            review.to_json
+            status 204
+        else
+            error 400, review.errors.to_json
+        end
+    rescue => e
+        error 400, e.message.to_json
+    end
+end
+
+delete '/review/:id' do
+    begin
+        review = Review.get(params[:id])
+        if(review.destroy!)
+            status 204
+        else
+            error 400, review.errors.to_json
+        end
+    rescue => e
+        error 400, e.message.to_json
+    end
 end
